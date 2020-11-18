cfea6d9bc46919d49e8c66af844772cb16c680f4
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2020-11-18 22:54:49 +0100

    ✅ Update public-api.ts

--- projects/ionic-stripe-checkout/src/public-api.ts
+++ projects/ionic-stripe-checkout/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ionic-stripe-checkout
  */
 
-export * from './lib/ionic-stripe-checkout.service';
-export * from './lib/ionic-stripe-checkout.component';
+export * from './lib/services/ionic-stripe-checkout.service';
+export * from './lib/components/ionic-stripe-checkout.component';
 export * from './lib/ionic-stripe-checkout.module';
